cda5dab31dc3a7b54c3dbcb3b3c9978279068e11
CountNick <meijer.nick1@gmail.com>
2019-05-13 20:22:08 +0200

    Added 404 route to index.js

--- server/index.js
+++ server/index.js
@@ -9,6 +9,12 @@ var app = express();
 
 app.use(express.static('static'));
 
+// Route not found (404)
+//Source: https://davidburgos.blog/how-to-handle-404-and-500-errors-on-expressjs/
+app.use(function(req, res, next) {
+    return res.status(404).send({ message: 'Route'+req.url+' Not found.' });
+  });
+  
     // Listen to port 5000
     app.listen(5000, function () {
         console.log('Dev app listening on port 5000!');
