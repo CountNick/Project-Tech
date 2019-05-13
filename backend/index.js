cd12a2b571680591db264546306690cbfa09da35
CountNick <meijer.nick1@gmail.com>
2019-05-13 11:11:23 +0200

    Installed express, and tried to set up a server

--- backend/index.js
+++ backend/index.js
@@ -1,6 +1,29 @@
-var camelCase = require('camelcase');
+/*var camelCase = require('camelcase');
 
 
-console.log(camelCase('foo-bar'));
+console.log(camelCase('foo-bar'));*/
+
+const express = require('express');
+
+const app = express();
+
+//app.use(express.static('./public'))
+app.use(express.static(__dirname + '/public'));
+    
+    app.get('/', function (req, res) {
+        res.send('Dit is de homepagina');
+    });
+
+    app.get('/login', function (req, res) {
+        res.send('Dit is de homepagina');
+    });
+
+    // Listen to port 5000
+    app.listen(5000, function () {
+        console.log('Dev app listening on port 5000!');
+    });
+
+
+
 
 
