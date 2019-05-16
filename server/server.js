03d2f2eca950ff6239b28e7f5fe42016efbb3a6d
CountNick <meijer.nick1@gmail.com>
2019-05-16 12:58:49 +0200

    Changed server.js and changed notfound.html to notfound.ejs

--- server/server.js
+++ server/server.js
@@ -11,15 +11,25 @@ app.get('/', home);
 
 
 function home(req, res) {
-    res.render('index')
+    res.render('index');
   }
+
+function login(req, res) {
+    res.render('login');
+}
+
+function notFound(req, res){
+    res.status(404).render('notfound');
+}
+
+app.use(notFound);
 
 
 // Route not found (404)
 //https://expressjs.com/en/starter/faq.html
-app.use(function (req, res, next) {
-    res.status(404).send("Sorry can't find that page!");
-  });
+//app.use(function (req, res, next) {
+//    res.status(404).send("Sorry can't find that page!");
+//  });
 
   
     // Listen to port 5000
