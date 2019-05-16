eec1b0b2d5b283bb0fcaaab741b6b00dd25fcb02
CountNick <meijer.nick1@gmail.com>
2019-05-16 11:24:11 +0200

    Changed name of index.js to server.js

--- server/server.js
+++ server/server.js
@@ -9,27 +9,16 @@ var app = express();
 
 app.use(express.static('static'));
 
+
+
 // Route not found (404)
 //https://expressjs.com/en/starter/faq.html
 app.use(function (req, res, next) {
     res.status(404).send("Sorry can't find that page!");
   });
+
   
     // Listen to port 5000
     app.listen(5000, function () {
         console.log('Dev app listening on port 5000!');
     });
-
-
-
-
-    
-
-
-
-
-
-
-
-
-
