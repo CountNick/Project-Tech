377084e6b67af1a790243dbec99d5a14eef77041
CountNick <meijer.nick1@gmail.com>
2019-05-20 22:01:22 +0200

    Installed slug and bodyparser via npm

--- server/server.js
+++ server/server.js
@@ -1,13 +1,27 @@
 var express = require('express');
+var slug = require('slug');
+var bodyParser = require('body-parser');
 
 var app = express();
 
+
+var data = 
+
+    {
+        id: "person1",
+        name: "Andrea",
+        age: "24",
+        bio:"Lorem Ipsum is simply dummy text of the printing and typesetting industry.Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum."
+    }
+
+
 app.use(express.static('static'));
+app.use(bodyParser.urlencoded({extended: true}));
 app.set('views', 'view/pages');
 app.set('view engine', 'ejs');
 
-
 app.get('/', home);
+app.post('/', add);
 app.get('/login', login);
 app.get('/changeprofile', changeProfile)
 app.get('/', form);
@@ -16,19 +30,19 @@ app.use(notFound);
 
 function home(req, res) {
     res.render('index',{
-        title: "Your profile"
-    });
-  }
+        data: data, 
+        pageTitle: data.name + "'s profile"});
+}
 
 function login(req, res) {
     res.render('login',{
-        title: "Login"
+        pageTitle: "Login"
     });
 }
 
 function changeProfile(req, res) {
     res.render('changeprofile',{
-        title: "Change profile"
+        pageTitle: "Change profile"
     });
 }
 
@@ -36,9 +50,21 @@ function form(req, res){
     res.render('changeprofile')
 }
 
+function add(req, res){
+    var id = slug(req.body.title).toLowerCase();
+
+    data.push({
+        id: id,
+        name: req.body.name
+
+    });
+
+    res.redirect('/' +id);
+}
+
 function notFound(req, res){
     res.status(404).render('notfound',{
-        title: "404"
+        pageTitle: "404"
     });
 }
 
