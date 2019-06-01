e37cdb7ff56391710155949e9ce70630991d143a
CountNick <meijer.nick1@gmail.com>
2019-06-01 22:20:54 +0200

    Added db model for mongoose

---
+++ models/user.js
@@ -0,0 +1,19 @@
+var mongoose = require('mongoose');
+
+
+
+var userSchema = new mongoose.Schema({
+  
+    id: String,
+
+    name: String,
+            
+    age: Number,
+
+    bio: String,
+
+    artists: Array
+  });
+
+  // eslint-disable-next-line no-unused-vars
+  var User = module.exports = mongoose.model('User', userSchema);
